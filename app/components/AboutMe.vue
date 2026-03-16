ad509434324fa4e8a26c068297e476ff4660aedd
Alex <avaleraprieto@gmail.com>
2026-03-16 12:20:45 +0100

    feat(issue19/about me): added an abbout me on home page

---
+++ app/components/AboutMe.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+<template>
+
+    <div class="h-fit leading-8 text-white font-bold">
+    <h1 class="mx-5 text-xl">About Me </h1>
+    <p class=" bg-indigo-800 m-5 p-5">
+        I am a 21 year old front-end developer, designer and tech enthusiast based in Madrid. I
+        specialize in building modern, scalable web applications with a focus on user experiece and
+        clean minimalistic aesthetic. My goal is to create experiences that are not only functional
+        but also visually stunning, a bridge between complex features and intuitive interfaces.
+    </p>
+    </div>
+   
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,13 @@
 <script setup>
 import Presentation from '~/components/Presentation.vue';
 import Techs from '~/components/Techs.vue';
-
+import AboutMe from '~/components/AboutMe.vue';
 </script>
 <template >
         <Presentation></Presentation>
-        <Techs></Techs>
-  
-    </template>
+        
+        <div class="grid grid-cols-2">
+            <AboutMe></AboutMe>      
+            <Techs></Techs>
+        </div>
+</template>
