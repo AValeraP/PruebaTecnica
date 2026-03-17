b7d9586201e69067169e6e1a977366fb84402e2a
Alex <avaleraprieto@gmail.com>
2026-03-17 11:43:02 +0100

    feat(issue 13) added contact information

---
+++ app/components/Contact/ContactCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+<template class="bg-purple-950">
+    <div class="m-4 p-4">
+        <h1 class="font-extrabold text-white">Let's build something
+    <span class="font-extrabold text-fuchsia-600">extraordinary</span>
+    together</h1>
+
+    <div class=" my-2 py-2 text-xs text-fuchsia-600">
+        I'm currently available for freelance work and full time opportunities.
+        Reach out if you want to collaborate on you next big project.
+    </div>
+
+    <div class="inline-flex flex-col  items-stretch">
+        <div class=" m-3 p-3 border border-fuchsia-950 shadow bg-fuchsia-950 rounded">
+            <h2 class="text-gray-300 font-normal text-xs m-2"> Email me</h2>
+            <p class="text-white font-normal m-2">avaleraprieto@gmail.com</p>
+        </div>
+        <div class=" m-3 p-3 border border-fuchsia-950 shadow bg-fuchsia-950 rounded">
+            <h2 class=" font-normal text-white text-xs m-2">My telephone number</h2>
+            <p class="text-white font-normal m-2">213123123</p>
+        </div>
+        <div class=" m-3 p-3 border border-fuchsia-950 shadow bg-fuchsia-950 rounded">
+            <h2 class=" font-normal text-white text-xs m-2">Location</h2>
+            <p class="text-white font-normal m-2">Madrid</p>
+        </div>
+    </div>
+
+    </div>
+
+</template>
